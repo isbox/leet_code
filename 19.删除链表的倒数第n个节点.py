86420a17f921262cc973368fe06d720bad41891a
box <thereisbox@foxmail.com>
2020-02-04 14:39:03 +0800

    feat: 19

--- 19.删除链表的倒数第n个节点.py
+++ 19.删除链表的倒数第n个节点.py
@@ -12,47 +12,47 @@ class ListNode:
         self.next = None
 
 # 解题思路：使用快慢指针
-# 快指针先行，领先慢指针n个长度，当快指针走到链表尽头时，此时慢指针后一位的位置即是要删除的位置
+# 快指针先行，领先慢指针n个长度，当快指针走到链表尽头时，此时慢指针的位置即是要删除的位置
 class Solution:
     def removeNthFromEnd(self, head, n):
         if n == 0:
             return head
 
-        current = head
+        if not head.next and n == 1 :
+            head.val = None
+            return None
+
         # 定义快指针
         fast_pointer = head
-        # 定义慢指针
+        # 定义慢指针，由于删除需要知道慢指针前一个是什么，所以由下方slow_prev_pointer记录
         slow_pointer = None
-        index = 0
-        while fast_pointer.next:
-            # 当快指针领先满指针到n值时，慢指针开始移动
-            if index >= n:
-                if slow_pointer:
-                    slow_pointer = slow_pointer.next
-                else:
-                    slow_pointer = head
-
+        # 记录前一个慢指针
+        slow_prev_pointer = None
+        # 由于快指针默认为头节点，所以已经先行一步，index初始值为1
+        index = 1
+        while fast_pointer:
             # 链表前进到下一位，步数(index)加1
             fast_pointer = fast_pointer.next
             index = index + 1
 
-        # 如果slow指针存在值且slow指针下一位也存在
-        if slow_pointer and slow_pointer.next:
-            # 将slow指针下一位指向下下位，从而将slow后以为值删除
-            slow_pointer.next = slow_pointer.next.next
+            # 当快指针领先满指针大于n值时，慢指针开始移动
+            if index > n:
+                if slow_pointer:
+                    # 记录前一个慢指针
+                    slow_prev_pointer = slow_pointer
+                    slow_pointer = slow_pointer.next
+                else:
+                    slow_pointer = head
+
+        if slow_pointer:
+            if slow_prev_pointer:
+                slow_prev_pointer.next = slow_prev_pointer.next.next
+            else:
+                # 如果slow_prev_pointer = None说明要删除的链表结点为头结点
+                # 参数无法重新赋值，用此方式相当与删除了头节点
+                return slow_pointer.next
 
         return head
-
-ls = ListNode(1)
-# ls.next = ListNode(2)
-# ls.next.next = ListNode(3)
-# ls.next.next.next = ListNode(4)
-# ls.next.next.next.next = ListNode(5)
-a = Solution()
-c = a.removeNthFromEnd(ls, 1)
-while ls:
-    print(ls.val)
-    ls = ls.next
 
 
 # @lc code=end
