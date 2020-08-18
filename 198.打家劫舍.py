888870ec7eba4e4846ee99c56d45b7eb092feb73
xh <zhanghaoran@biaoguoworks.com>
2020-08-18 16:47:35 +0800

    feat: 198打家劫舍

--- 198.打家劫舍.py
+++ 198.打家劫舍.py
@@ -1,4 +1,4 @@
-#
+# -*- encoding:utf-8 -*-
 # @lc app=leetcode.cn id=198 lang=python3
 #
 # [198] 打家劫舍
@@ -7,27 +7,24 @@
 # @lc code=start
 class Solution:
     def rob(self, nums):
+        if not nums:
+            return 0
         numsLen = len(nums)
         if numsLen == 0:
-            return 0;
+            return 0
         if numsLen == 1:
             return nums[0]
-        
 
-        stealed = {}
-        stealIndex = 0
-        totalCount = 0
+        prev = nums[0]
+        cur = max(prev, nums[1])
 
-        while True:
-            if stealed.get(stealIndex - 1) and stealed.get(stealIndex + 1):
-                return totalCount
-            else if 
-            else:
-                stealed[stealIndex] = True
-                totalCount += nums[stealIndex]
+        for i in range(2, numsLen):
+            print(i)
+            temp = cur
+            cur = max(prev + nums[i], cur)
+            prev = temp
 
-        
-        return max(oddCount, evenCount)
+        return cur
             
 # @lc code=end
 
