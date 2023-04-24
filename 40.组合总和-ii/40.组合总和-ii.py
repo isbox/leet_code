1a6e4e859b1c0a2528665285a61068d9dbf330eb
星河 <thereisbox@foxmail.com>
2023-04-24 11:25:29 +0800

    feat: 题目4

--- 40.组合总和-ii/40.组合总和-ii.py
+++ 40.组合总和-ii/40.组合总和-ii.py
@@ -1,7 +1,7 @@
 # -*- encoding:utf-8 -*-
-# @lc app=leetcode.cn id=39 lang=python3
+# @lc app=leetcode.cn id=40 lang=python3
 #
-# [39] 组合总和
+# [40] 组合总和 II
 #
 
 from functools import reduce
@@ -9,10 +9,10 @@ from functools import reduce
 # @lc code=start
 class Solution:
     target = 0
-    result = []
+    result = {}
     candidates = []
 
-    def combinationSum(self, candidates, target):
+    def combinationSum2(self, candidates, target):
         if not candidates or len(candidates) == 0:
             return []
 
@@ -20,12 +20,12 @@ class Solution:
             return candidates
         
         # 初始化
-        self.result = []
-        self.candidates = candidates
+        self.result = {}
+        candidates.sort()
         self.target = target
-
+        self.candidates = candidates
         self.solution(target, [], 0)
-        return self.result
+        return list(map(lambda x: self.result[x], self.result))
 
     def solution(self, target, combins, index):    
         if index >= len(self.candidates):
@@ -33,7 +33,7 @@ class Solution:
         
         # 到0跳出
         if target == 0:
-            self.result.append(combins)
+            self.result[str(combins)] = combins
             return None
 
         totalNow = reduce(lambda x, y: x + y, combins, 0)
@@ -46,7 +46,12 @@ class Solution:
             copyCombins = combins[:]
             copyCombins.append(self.candidates[index])
             # 尝试同层搜索
-            self.solution(subtractNum, copyCombins, index)
+            self.solution(subtractNum, copyCombins, index + 1)
             
-# @lc code=end
 
+a = Solution()
+# b = a.combinationSum([2, 3, 6, 7], 7)
+b = a.combinationSum2([2,5,2,1,2], 5)
+print(b)
+
+# @lc code=end
